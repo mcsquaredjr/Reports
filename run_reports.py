5434f225a2a8c58ea49eca6993d3291a481ba3f5
Serge Boyko <serge.boyko@gmail.com>
2013-09-16 09:15:47 -0600

    changed datafield css

--- run_reports.py
+++ run_reports.py
@@ -100,7 +100,7 @@ def serve(requestedfile):
     
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8002))
     app.run(host='0.0.0.0', port=port)
 
 
